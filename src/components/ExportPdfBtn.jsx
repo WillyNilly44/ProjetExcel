22fb7887e997393860485cf66809cf7a17db96ed
WillyNilly44 <94925679+WillyNilly44@users.noreply.github.com>
2025-05-16 09:40:53 -0400

    Update ExportPdfBtn.jsx

--- src/components/ExportPdfBtn.jsx
+++ src/components/ExportPdfBtn.jsx
@@ -84,6 +84,7 @@ export default function ExportPdfBtn({ adminNotes = [] }) {
       body: body,
       willDrawCell: function (data) {
         if (data.row.raw?.isAdminNote) {
+          console.log("here");
           data.cell.styles.fillColor = [255, 250, 205]; // jaune clair
           data.cell.styles.textColor = [200, 0, 0];     // rouge foncé
         }
